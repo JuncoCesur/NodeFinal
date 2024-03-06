637fe24ccb3886a6697ea5bb0d25a9a394607c87
JuncoCesur <javierdeljuncocesur@gmail.com>
2024-03-06 18:38:24 +0100

    Commit Node Final

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import {dirname, join} from 'path';
+import { fileURLToPath } from 'url';
+import indexRoutes from './routes/index.js';
+
+
+
+const port = 3000
+const app =express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(join(__dirname,'views'));
+
+
+
+app.set('view engine','ejs');
+app.set('views',join(__dirname,'views'));
+
+
+app.use(indexRoutes);
+app.use(express.static(join(__dirname,'public')));
+
+
+app.listen(process.env.PORT || port)
+console.log('Escuchando por el puerto '+port+'...')
+
+
